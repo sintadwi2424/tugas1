8c4ed1a586d3a20241b9c8367cce3c6becc8269b
sintadwi2424 <68579286+sintadwi2424@users.noreply.github.com>
2020-10-13 15:56:19 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'SiswaController@index');
+Route::post('/siswa/import_excel', 'SiswaController@import');
+
+
+Route::get('/tampildata', 'SiswaController@tampildata')->name('tampildata');
